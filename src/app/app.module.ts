db46ae17981d0f5c6f4796baf21e143abc59f27b
mrandycode <cevallos.andy@gmail.com>
2022-11-13 15:04:23 -0400

    Firebase Integration, css modify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -8,19 +8,12 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { SharedModule } from './shared/shared.module';
 
+const BASIC_MODULES = [FormsModule, ReactiveFormsModule, BrowserModule];
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    CoreModule,
-  ],
+  declarations: [AppComponent, HomeComponent, FooterComponent],
+  imports: [BASIC_MODULES, AppRoutingModule, SharedModule, CoreModule],
+  exports: [BASIC_MODULES],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+const BASIC_MODULES = [FormsModule, ReactiveFormsModule, BrowserModule];
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
-  exports: [NavbarComponent],
+  imports: [BASIC_MODULES, CommonModule],
+  exports: [BASIC_MODULES, NavbarComponent],
 })
 export class SharedModule {}
